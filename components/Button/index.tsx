b41dbb830a3d935aa36d69a7a504e5dc33aab474
Stone Tao <stoneztao@gmail.com>
2019-11-05 10:18:15 -0800

    Initial Commit (#1)
    * Initial Commit
    
    * Colors for buttons and styling
    
    * Bundle analyzerand colors
    
    * 2.2.0
    
    * Reduce size
    
    * 2.2.1
    
    * Final reductions in package size

---
+++ components/Button/index.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {Button as AntdButton} from 'antd';
+
+import './styles.less';
+
+/* Default Button is medium sized */
+export function Button(props: any) {
+
+  let pseudoProps = {...props};
+  if (props.type == 'primary-outline') {
+    pseudoProps.className += ' d4sdbutton d4sdbutton-primary d4sdbutton-outline';
+  }
+  else if (props.type == 'secondary-outline') {
+    pseudoProps.className += ' d4sdbutton d4sdbutton-secondary d4sdbutton-outline';
+  }
+  else if (props.type == 'primary') {
+    pseudoProps.className += ' d4sdbutton d4sdbutton-primary';
+  }
+  else if (props.type == 'secondary') {
+    pseudoProps.className += ' d4sdbutton d4sdbutton-secondary';
+  }
+  else {
+    pseudoProps.className += ' d4sdbutton d4sdbutton-primary';
+  }
+  if (props.size == 'medium') {
+    pseudoProps.className += ' d4sdbutton-medium'
+  }
+  else if (props.size == 'large') {
+    pseudoProps.className += ' d4sdbutton-large'
+  }
+  else if (props.size == 'small') {
+    pseudoProps.className += ' d4sdbutton-small'
+  }
+  else {
+    pseudoProps.className += ' d4sdbutton-medium'
+  }
+  return (
+    <div className = "d4sdbutton-wrapper">
+      <AntdButton {...pseudoProps}>{props.children}</AntdButton>
+    </div>
+  )
+}
+
+Button.Google = function(props: any) {
+
+  let pseudoProps = {...props};
+  pseudoProps.className += ' d4sdbutton-google';
+  return (
+    <div className = "d4sdbutton-wrapper">
+      <AntdButton {...pseudoProps}>{props.children}</AntdButton>
+    </div>
+  )
+}
+
+Button.Facebook = function(props: any) {
+
+  let pseudoProps = {...props};
+  pseudoProps.className += ' d4sdbutton-facebook';
+  return (
+    <div className = "d4sdbutton-wrapper">
+      <AntdButton {...pseudoProps}>{props.children}</AntdButton>
+    </div>
+  )
+}
