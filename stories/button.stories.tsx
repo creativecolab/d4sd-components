d4324982d7dc6f984a46397b4fb19d2ef2b245c7
StoneT2000 <stoneztao@gmail.com>
2019-11-07 10:45:12 -0800

    Examples

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -1,9 +1,56 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import 'antd/dist/antd.css'
+import './style.less';
 import { Button, Radio } from "../";
+
 storiesOf("Button", module)
-  .add("with text", () => <Button>Hello Button</Button>)
-  .add("with some emoji", () => <Button>😀 😎 👍 💯</Button>);
+  .add("All", () =>
+    <div className="storycontainer">
+    <div className="story-module">
+        <h3>Small Buttons (fixed)</h3>
+        <Button type='primary-outline' size='small'>PREVIOUS</Button>
+        <Button type='primary' size='small'>NEXT</Button>
+        <Button type='primary' size='small'>VERY STRETCHED OUT BUTTON</Button>
+        <Button type='secondary-outline' size='small'>PREVIOUS</Button>
+        <Button type='secondary' size='small'>NEXT</Button>
+        <Button type='secondary' size='small'>VERY STRETCHED OUT BUTTON</Button>
+    </div>
+      <div className="story-module">
+          <h3>Medium Buttons (sizable)</h3>
+          <Button type='primary-outline'>PREVIOUS</Button>
+          <Button type='primary'>NEXT</Button>
+          <Button type='primary'>VERY STRETCHED OUT BUTTON</Button>
+          <Button type='secondary-outline'>PREVIOUS</Button>
+          <Button type='secondary'>NEXT</Button>
+          <Button type='secondary'>VERY STRETCHED OUT BUTTON</Button>
+      </div>
+      <div className="story-module">
+          <h3>Large Buttons (fixed)</h3>
+          <Button type='primary-outline' size='large'>PREVIOUS</Button>
+          <Button type='primary' size='large'>NEXT</Button>
+          <Button type='primary' size='large'>VERY STRETCHED OUT BUTTON LIKE REALLY STRETCHED</Button>
+          <Button type='secondary-outline' size='large'>PREVIOUS</Button>
+          <Button type='secondary' size='large'>NEXT</Button>
+          <Button type='secondary' size='large'>VERY STRETCHED OUT BUTTON LIKE REALLY STRETCHED</Button>
+      </div>
+    </div>)
 storiesOf("Radio", module)
-  .add("with text", () => <Radio>Hello Button</Radio>)
-  .add("with some emoji", () => <Radio>😀 😎 👍 💯</Radio>);
+  .add("Examples", () =>
+    <div className="storycontainer">
+    <div className="story-module">
+    <h3>Exclusive buttons</h3>
+      <Radio.Group>
+        <Radio value="accept">I accept the terms</Radio>
+        <br />
+        <Radio value="no">I don't accept the terms</Radio>
+      </Radio.Group>
+    </div>
+    <div className="story-module">
+    <h3>Not-Exclusive buttons</h3>
+        <Radio value="accept">I accept the terms</Radio>
+        <br />
+        <Radio value="no">I don't accept the terms</Radio>
+        <Radio disabled={true}>Disabled</Radio>
+    </div>
+    </div>)
