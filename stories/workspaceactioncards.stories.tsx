d2bcc15e25e870fce8e01772089793d0c8381550
StoneT2000 <stoneztao@gmail.com>
2019-12-03 15:44:56 -0800

    Update workspaceactioncards.stories.tsx

--- stories/workspaceactioncards.stories.tsx
+++ stories/workspaceactioncards.stories.tsx
@@ -6,7 +6,9 @@ const cards = [
   {title:"Discover a Challenge", desc:"Submit your problem description and solution proposal here", closed:false, buttonTitle:"JOIN DISCUSSION", buttonHandleClick:() => {
     message.success("Action clicked");
   }},
-  {title:"Register your Team", desc: "Learn about the challenges surrounding San Diego", closed:false,dueDate: new Date("01/25/2020, 11:59:00 PM"), submitted:true, buttonTitle:"SEARCH FOR TEAMMATES"},
+  {title:"Register your Team", desc: "Learn about the challenges surrounding San Diego", closed:false,dueDate: new Date("01/25/2020, 11:59:00 PM"), submitted:true, buttonTitle:"SEARCH FOR TEAMMATES", buttonHandleClick:() => {
+    message.success("HELLO");
+  }},
   {title:"Submit a Preliminary Concept", desc:"Submit your problem description and solution proposal here", closed:false, dueDate: new Date("12/12/2019, 11:59:00 PM"), submitted:false, buttonTitle:"SOMETHING"},
   {title: "Request and Provide Feedback", desc:"Feedback will be available once you’ve made a submission", closed:true, dueDate: new Date("12/18/2019, 11:59:00 PM"), submitted:false},
   {title: "Submit Your Team’s Final Concept", desc:"Final submission will be available once you’ve made a submission", closed:true, dueDate: new Date("12/24/2019, 11:59:00 PM"), submitted:false},
