db3ecb98d93045a352cbd5024be4d14a4efa8192
StoneT2000 <stoneztao@gmail.com>
2019-12-23 10:52:28 +0800

    Added feedback counters and copy links, and some more typesafe prop moving

--- components/WorkspaceActionCard/index.tsx
+++ components/WorkspaceActionCard/index.tsx
@@ -3,16 +3,32 @@ import {Progress, Button, Collapse} from '../../';
 const { Panel } = Collapse;
 import './styles.less'
 
-export function WorkspaceActionCard(props: any) {
-  const card = props.card;
+export function WorkspaceActionCard(props:
+  {
+    card: {
+      submitState?:string | "saved" | "none" | "submitted" | "waitForFeedback" | "feedback",
+      closed?:boolean,
+      title?:string,
+      shortDesc?: string,
+      dueDate?: Date,
+      startDate?: Date,
+      active?: boolean,
+      buttonTitle?: string,
+      buttonHandleClick?: Function,
+      descHTML?: string,
+      desc?: string,
+      feedbacklink?: string,
+      feedback?: number
+    }
+  }
+) {
+  let card = props.card;
   const calcPercentTimeLeft = (dueDate: Date, startDate: Date) => {
     let currDate = new Date();
     // @ts-ignore
     let timeLeft: number = (dueDate - currDate);
     // @ts-ignore
     let totalTime: number = (dueDate - startDate);
-
-    let hoursLeft = timeLeft/(1000 * 60 *60);
     return (timeLeft / totalTime) * 100
   }
   const calcDaysLeft = (dueDate: Date, startDate: Date) => {
@@ -23,6 +39,33 @@ export function WorkspaceActionCard(props: any) {
     let daysLeft = timeLeft/(1000 * 60 * 60 * 24);
     return Math.floor(daysLeft).toFixed(0);
   }
+  const [linkToFeedback, setLinkToFeedback] = React.useState('LINK GOES HERE');
+
+  /* eslint-disable */
+  const setURL = (): void => {
+    const inputField = copyRef.current;
+    // @ts-ignore
+    if (inputField) inputField.value = linkToFeedback;
+  };
+
+  const copyURL = (): void => {
+    const copyText = copyRef.current;
+    // @ts-ignore
+    if (copyText) {
+      copyText.select();
+      // @ts-ignore
+      copyText.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+    }
+  };
+  const copyRef: React.RefObject<HTMLInputElement> = React.createRef();
+  React.useEffect(() => {
+    console.log(copyRef)
+    let link = card.feedbacklink ? card.feedbacklink : "";
+    setLinkToFeedback(link);
+    setURL();
+  // eslint-disable-next-line
+});
   return (
     <Collapse accordion className="WorkspaceActionCardWrapper">
     <Panel
@@ -30,21 +73,52 @@ export function WorkspaceActionCard(props: any) {
     header={<div className={!card.closed ? "WorkspaceActionCard" : "WorkspaceActionCard closed"}>
     <h2 className="card-title">{card.title}</h2>
     <p className="card-short-desc">{card.shortDesc}</p>
-      { !card.closed && card.dueDate && card.startDate && (card.submitted || !card.submitted) &&
+      { !card.closed && card.dueDate && card.startDate && (card.submitState || !card.submitState) &&
       <div>
-        <div className="card-status">{card.submitted ? <span className="submitted">SUBMITTED </span> : <span className="no-submit">NO SUBMISSION</span>}</div>
-        <Progress percent={calcPercentTimeLeft(card.dueDate, card.startDate)} status={card.submitted ? "success" : undefined} className={card.submitted ? "progressBar success" : "progressBar warning"}/>
-        <div className="card-dates">DUE:
-          <span className={card.submitted ? "due-date success" : "due-date warning"}>{" " + card.dueDate.toLocaleDateString() + " " + card.dueDate.toLocaleTimeString()}</span>
-          <span className="days-left">{calcDaysLeft(card.dueDate, card.startDate)} DAYS LEFT</span>
+        <div className="card-status">
+        {card.submitState === "submitted" && <span className="submitted">SUBMITTED </span>}
+        {card.submitState === "saved" && <span className="inprogress-submit">PROGRESS SAVED</span>}
+        {card.submitState === "none" && <span className="no-submit">NO SUBMISSION</span>}
+        {card.submitState === "waitForFeedback" && <span className="feedback-submit">SUBMITTED, WAITING FOR FEEDBACK</span>}
+        {card.submitState === "feedback" &&
+          <div className={card.feedback! > 0 ? "feedback-new" : "feedback-none"}>
+            <div className="feedback-count"><span>{card.feedback}</span></div>
+            <span className="feedback-text">NEW FEEDBACK</span>
+          </div>
+        }
         </div>
-
+        {(card.submitState !== "waitForFeedback" && card.submitState !== "feedback") &&[
+        <Progress
+          percent={calcPercentTimeLeft(card.dueDate, card.startDate)}
+          status={card.submitState === "submitted" ? "success" : undefined}
+          className={card.submitState === "submitted" ? "progressBar success" : (card.submitState === "saved" ? "progressBar progress" : "progressBar warning")}
+          />,
+        <div className="card-dates">DUE:
+          <span className={card.submitState === "submitted" ? "due-date success" : (card.submitState === "saved" ? "due-date progress" : "due-date warning")}>{" " + card.dueDate.toLocaleDateString() + " " + card.dueDate.toLocaleTimeString()}</span>
+          <span className="days-left">{calcDaysLeft(card.dueDate, card.startDate)} DAYS LEFT</span>
+        </div>]
+        }
       </div> }
+      {card.feedbacklink &&
+        <div className="share-feedback-wrapper">
+        <p>Share your unique url to ask for feedback</p>
+        <div className="copy-container">
+          <input className="copy-link" id="copyLink" ref={copyRef} />
+          <Button
+            className="copy-btn" type="primary-outline" size="small"
+            onClick={(): void => { copyURL(); }}
+          >
+            COPY URL
+          </Button>
+        </div>
+        </div>
+      }
       {card.buttonTitle && <div className="btn-wrapper">
       {card.active === true ? <Button type="primary-outline" size="large" onClick={card.buttonHandleClick}>{card.buttonTitle}</Button> :
                      <Button type="primary-outline" size="large" disabled onClick={card.buttonHandleClick}>{card.buttonTitle}</Button>
                    }
       </div> }
+
     </div>}
       className="panelCard"
     >
--- stories/workspaceactioncards.stories.tsx
+++ stories/workspaceactioncards.stories.tsx
@@ -6,11 +6,14 @@ const cards = [
   {title:"Discover a Challenge", desc:"Submit your problem description and solution proposal here", closed:false,shortDesc: "Something here goes here ... ",active:true, buttonHandleClick:() => {
     message.success("Action clicked");
   }},
-  {title:"Register your Team", desc: "Learn about the challenges surrounding San Diego", closed:false,dueDate: new Date("01/25/2020, 11:59:00 PM"), submitted:true, buttonTitle:"SEARCH FOR TEAMMATES", active:true, buttonHandleClick:() => {
+  {title:"Register your Team", desc: "Learn about the challenges surrounding San Diego", closed:false,dueDate: new Date("01/25/2020, 11:59:00 PM"),startDate: new Date("12/16/2019, 11:59:00 PM"), submitState:"submitted", buttonTitle:"SEARCH FOR TEAMMATES", active:true, buttonHandleClick:() => {
     message.success("HELLO");
   }},
-  {title:"Submit a Preliminary Concept", desc:"Submit your problem description and solution proposal here", shortDesc: "Something here goes here ... ",active:false, closed:false, dueDate: new Date("12/12/2019, 11:59:00 PM"), submitted:false, buttonTitle:"SOMETHING"},
-  {title: "Request and Provide Feedback", desc:"Feedback will be available once you’ve made a submission", closed:true, dueDate: new Date("12/18/2019, 11:59:00 PM"), submitted:false},
+  {title:"Submit a Preliminary Concept", desc:"Submit your problem description and solution proposal here", shortDesc: "Something here goes here ... ",active:false, closed:false, dueDate: new Date("02/03/2020, 11:59:00 PM"),startDate: new Date("12/01/2019, 11:59:00 PM"),submitState:"saved", buttonTitle:"SOMETHING"},
+  {title: "Request and Provide Feedback", desc:"Feedback will be available once you’ve made a submission", closed:false, dueDate: new Date("02/02/2020, 11:59:00 PM"), startDate: new Date("12/16/2019, 11:59:00 PM"), submitState:"none", feedbacklink:"https://www.bing.com"},
+  {title: "Request and Provide Feedback 2", desc:"Feedback will be available once you’ve made a submission", closed:false, dueDate: new Date("02/02/2020, 11:59:00 PM"), startDate: new Date("12/16/2019, 11:59:00 PM"), submitState:"waitForFeedback", feedbacklink:"https://www.bing.com"},
+  {title: "Request and Provide Feedback 3", desc:"Feedback will be available once you’ve made a submission", closed:false, dueDate: new Date("02/02/2020, 11:59:00 PM"), startDate: new Date("12/16/2019, 11:59:00 PM"), submitState:"feedback", feedback:3, feedbacklink:"https://www.bing.com"},
+  {title: "Request and Provide Feedback 4", desc:"Feedback will be available once you’ve made a submission", closed:false, dueDate: new Date("02/02/2020, 11:59:00 PM"), startDate: new Date("12/16/2019, 11:59:00 PM"), submitState:"feedback", feedback:0, feedbacklink:"https://www.bing.com"},
   {title: "Submit Your Team’s Final Concept", desc:"Final submission will be available once you’ve made a submission", closed:true, dueDate: new Date("12/24/2019, 11:59:00 PM"), submitted:false},
   {title:"View Community Voting Results", desc:"Judging will be available at the end of the competition", closed:true, dueDate: new Date("01/24/2020, 11:59:00 PM"), submitted:false}
 ]
