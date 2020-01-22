2fa65dc823e4495e44d8c4a5726ea512118426d5
Lee Liang <ltliang1@gmail.com>
2020-01-22 12:19:23 -0800

    fix Header/index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,31 @@
 import * as React from 'react';
 import './styles.less';
-const arrow = require("./arrow.svg") as string;
+import arrow from './arrow.svg';
 export function Header(props: any) {
-  const classes = "d4sdworkspace-header " + props.className;
-const listItems = props.teamPictures ? props.teamPictures.map((pic: any) =>
-  <img src={pic} className="team-member-pic"></img>
-) : undefined;
+  const classes = 'd4sdworkspace-header ' + props.className;
+  const listItems = props.teamPictures
+    ? props.teamPictures.map((pic: any) => (
+        <img src={pic} className="team-member-pic"></img>
+      ))
+    : undefined;
   return (
-    <div className={classes} >
-      {props.back && <div className="go-back-bar" onClick={props.handleBackClick}><img src={arrow} className="go-back-arrow"></img><span className="go-back">{props.back}</span></div>}
+    <div className={classes}>
+      {props.back && (
+        <div className="go-back-bar" onClick={props.handleBackClick}>
+          <img src={arrow} className="go-back-arrow"></img>
+          <span className="go-back">{props.back}</span>
+        </div>
+      )}
       <h1 className="header-team-title">{props.teamTitle}</h1>
       <h1 className="header-title">{props.title}</h1>
       <div className="team-info">
         {listItems}
-        {listItems && <span className="see-team" onClick={props.handleTeamClick}>See Team</span>}
+        {listItems && (
+          <span className="see-team" onClick={props.handleTeamClick}>
+            See Team
+          </span>
+        )}
       </div>
     </div>
-  )
+  );
 }
