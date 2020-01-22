2c78f56c7f0bc8c8fd162975103afb3c30599979
StoneT2000 <stoneztao@gmail.com>
2020-01-22 12:54:15 -0800

    update cards

--- components/WorkspaceActionCard/index.tsx
+++ components/WorkspaceActionCard/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import {Progress, Button, Collapse} from '../../';
-const { Panel } = Collapse;
+import {Progress, Button} from '../../';
 import './styles.less'
 
 export function WorkspaceActionCard(props:
@@ -23,6 +22,8 @@ export function WorkspaceActionCard(props:
   }
 ) {
   let card = props.card;
+  let [collapsed, setCollpased] = React.useState(true);
+
   const calcPercentTimeLeft = (dueDate: Date, startDate: Date) => {
     let currDate = new Date();
     // @ts-ignore
@@ -63,68 +64,67 @@ export function WorkspaceActionCard(props:
     console.log(copyRef)
     let link = card.feedbacklink ? card.feedbacklink : "";
     setLinkToFeedback(link);
+    setCollpased(true);
     setURL();
   // eslint-disable-next-line
 });
   return (
-    <Collapse accordion className="WorkspaceActionCardWrapper">
-    <Panel
-    key="1"
-    header={<div className={!card.closed ? "WorkspaceActionCard" : "WorkspaceActionCard closed"}>
-    <h2 className="card-title">{card.title}</h2>
-    <p className="card-short-desc">{card.shortDesc}</p>
-      { !card.closed && card.dueDate && card.startDate && (card.submitState || !card.submitState) &&
-      <div>
-        <div className="card-status">
-        {card.submitState === "submitted" && <span className="submitted">SUBMITTED </span>}
-        {card.submitState === "saved" && <span className="inprogress-submit">PROGRESS SAVED</span>}
-        {card.submitState === "none" && <span className="no-submit">NO SUBMISSION</span>}
-        {card.submitState === "waitForFeedback" && <span className="feedback-submit">SUBMITTED, WAITING FOR FEEDBACK</span>}
-        {card.submitState === "feedback" &&
-          <div className={card.feedback! > 0 ? "feedback-new" : "feedback-none"}>
-            <div className="feedback-count"><span>{card.feedback}</span></div>
-            <span className="feedback-text">NEW FEEDBACK</span>
+    <div className="WorkspaceActionCardWrapper">
+        <div className={!card.closed ? "WorkspaceActionCard" : "WorkspaceActionCard closed"}>
+          <div className="top" onClick={()=>{
+            setCollpased(!collapsed);
+          }}>
+            <h2 className="card-title">{card.title}</h2>
+            <div className="arrow">></div>
           </div>
-        }
-        </div>
-        {(card.submitState !== "waitForFeedback" && card.submitState !== "feedback") &&[
-        <Progress
-          percent={calcPercentTimeLeft(card.dueDate, card.startDate)}
-          status={card.submitState === "submitted" ? "success" : undefined}
-          className={card.submitState === "submitted" ? "progressBar success" : (card.submitState === "saved" ? "progressBar progress" : "progressBar warning")}
-          />,
-        <div className="card-dates">DUE:
-          <span className={card.submitState === "submitted" ? "due-date success" : (card.submitState === "saved" ? "due-date progress" : "due-date warning")}>{" " + card.dueDate.toLocaleDateString() + " " + card.dueDate.toLocaleTimeString()}</span>
-          <span className="days-left">{calcDaysLeft(card.dueDate, card.startDate)} DAYS LEFT</span>
-        </div>]
-        }
-      </div> }
-      {card.feedbacklink &&
-        <div className="share-feedback-wrapper">
-        <p>Share your unique url to ask for feedback</p>
-        <div className="copy-container">
-          <input className="copy-link" id="copyLink" ref={copyRef} />
-          <Button
-            className="copy-btn" type="primary-outline" size="small"
-            onClick={(): void => { copyURL(); }}
-          >
-            COPY URL
-          </Button>
-        </div>
-        </div>
-      }
-      {card.buttonTitle && <div className="btn-wrapper">
-      {card.active === true ? <Button type="primary-outline" size="large" onClick={card.buttonHandleClick}>{card.buttonTitle}</Button> :
-                     <Button type="primary-outline" size="large" disabled onClick={card.buttonHandleClick}>{card.buttonTitle}</Button>
-                   }
-      </div> }
+          {collapsed ? <p className="card-short-desc">{card.shortDesc}</p> : card.descHTML && <p dangerouslySetInnerHTML={{__html:card.descHTML}}></p>}
+            { !card.closed && card.dueDate && card.startDate && (card.submitState || !card.submitState) &&
+            <div>
+              <div className="card-status">
+              {card.submitState === "submitted" && <span className="submitted">SUBMITTED</span>}
+              {card.submitState === "saved" && <span className="inprogress-submit">PROGRESS SAVED</span>}
+              {card.submitState === "none" && <span className="no-submit">NO SUBMISSION</span>}
+              {card.submitState === "waitForFeedback" && <span className="feedback-submit">SUBMITTED, WAITING FOR FEEDBACK</span>}
+              {card.submitState === "feedback" &&
+                <div className={card.feedback! > 0 ? "feedback-new" : "feedback-none"}>
+                  <div className="feedback-count"><span>{card.feedback}</span></div>
+                  <span className="feedback-text">NEW FEEDBACK</span>
+                </div>
+              }
+              </div>
+              {(card.submitState !== "waitForFeedback" && card.submitState !== "feedback") &&[
+              <Progress
+                percent={calcPercentTimeLeft(card.dueDate, card.startDate)}
+                status={card.submitState === "submitted" ? "success" : undefined}
+                className={card.submitState === "submitted" ? "progressBar success" : (card.submitState === "saved" ? "progressBar progress" : "progressBar warning")}
+                />,
+              <div className="card-dates">DUE:
+                <span className={card.submitState === "submitted" ? "due-date success" : (card.submitState === "saved" ? "due-date progress" : "due-date warning")}>{" " + card.dueDate.toLocaleDateString() + " " + card.dueDate.toLocaleTimeString()}</span>
+                <span className="days-left">{calcDaysLeft(card.dueDate, card.startDate)} DAYS LEFT</span>
+              </div>]
+              }
+            </div> }
+            {card.feedbacklink &&
+              <div className="share-feedback-wrapper">
+              <p>Share your unique url to ask for feedback</p>
+              <div className="copy-container">
+                <input className="copy-link" id="copyLink" ref={copyRef} />
+                <Button
+                  className="copy-btn" type="primary-outline" size="small"
+                  onClick={(): void => { copyURL(); }}
+                >
+                  COPY URL
+                </Button>
+              </div>
+              </div>
+            }
+            {card.buttonTitle && <div className="btn-wrapper">
+            {card.active === true ? <Button type="primary-outline" size="large" onClick={card.buttonHandleClick}>{card.buttonTitle}</Button> :
+                           <Button type="primary-outline" size="large" disabled onClick={card.buttonHandleClick}>{card.buttonTitle}</Button>
+                         }
+            </div> }
+      </div>
 
-    </div>}
-      className="panelCard"
-    >
-    {card.descHTML && <p dangerouslySetInnerHTML={{__html:card.descHTML}}></p>}
-    {card.desc && <p>{card.desc}</p>}
-    </Panel>
-    </Collapse>
+    </div>
   )
 }
