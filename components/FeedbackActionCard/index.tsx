07759500439b95c51f5074768749c505c9719035
leestanley <stanleylee91776@gmail.com>
2020-01-29 13:13:56 -0800

    finalized

--- components/FeedbackActionCard/index.tsx
+++ components/FeedbackActionCard/index.tsx
@@ -13,11 +13,13 @@ export function FeedbackActionCard(props: {
   return (
     <div className="FeedbackActionCardWrapper">
       <div className="FeedbackActionCard">
-        <h1>Feedback From {card.name}</h1>
-        <p>{card.dateBack!.toDateString()}</p>
+        <div className="text">
+          <h1>Feedback From {card.name}</h1>
+          <p>{card.dateBack!.toDateString()}</p>
+        </div>
         <a href={card.feedbacklink}>
           <Button className="copy-btn" type="primary-outline" size="small">
-            COPY URL
+            VIEW FEEDBACK
           </Button>
         </a>
       </div>
